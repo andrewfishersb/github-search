08bfcaa3753a9df00de6627cf04687b6995efb7e
Andrew Fisher <AndrewFisherSB@gmail.com>
2016-10-14 11:29:01 -0700

    submit a user name and can see basic information such as their name,date,location

--- js/github-app-interface.js
+++ js/github-app-interface.js
@@ -1,12 +1,50 @@
 var User = require("./../js/user.js").userModule;
+
+var displayUser = function(username,name,avatar,created,location,url,hireable){
+  $("all-info").empty();
+  $("#pic").append("<img src='"+ avatar+ "'alt='users profile picture' />");
+  $("#user").append("<h2>"+name+" / <a href='"+url+"'>"+username+"</a></h2>");
+  $("#date").append("<h4>Date Created: "+created+"<h4>");
+  if(location){
+    $("#location").append("<h4>Location: "+location+"<h4>");
+  }
+  if(hireable){
+    $("#hire").append("<h5>Available for hire</h5>");
+  }else{
+    $("#hire").append("<h5>Currently Employed/ not looking</h5>");
+  }
+}
+
+
+
+
 $(document).ready(function(){
   var username = new User();
-  username.getRepos();
   $("#search").submit(function(event){
     event.preventDefault();
     var user = $("#username").val();
-    console.log(user);
-    $("#display").append("<h1>"+user+"</h1>");
+    // username.getRepos(user,displayUser);
+    username.getUserInformation(user, displayUser);
   });
 
 });
+
+//Ideas
+//show user searhed for
+  //button to show followers or following, each of which has a button to show more information like of their repos
+
+//avatar_url
+//created
+//followers + url
+//following + url
+//location
+//name
+//url
+//url to repo
+
+
+//possibly display repos
+  // languages
+  // name
+  //html_url
+  //description
--- js/user.js
+++ js/user.js
@@ -4,10 +4,25 @@ function User(){
  console.log("hello");
 }
 
-User.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/repos?access_token=' + apiKey).then(function(response){
-    $("#display").append("<li>API Key:"+apiKey+"<li>");
-    // $("#display").append("<li>User: " +  +"<li>");
+User.prototype.getUserInformation = function (username, displayFunction) {
+  $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
+    displayFunction(username,response.name, response.avatar_url, response.created_at,response.location,response.html_url,response.hireable);
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON);
+  });
+};
+
+
+
+
+
+
+
+//seperate method to get follower information
+User.prototype.getRepos = function(username, displayFunction){
+  $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(response){
+    displayFunction(username,response.followers);
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
@@ -17,3 +32,97 @@ User.prototype.getRepos = function(){
 
 exports.userModule = User;
 // f2f8bb3db6ffc1a7de0808320421143b7ce7835d
+//https://api.github.com/users/repos?access_token=
+
+
+// Object {login: "andrewfishersb", id: 17396138, avatar_url: "https://avatars.githubusercontent.com/u/17396138?v=3", gravatar_id: "", url: "https://api.github.com/users/andrewfishersb"…}
+// avatar_url
+// :
+// "https://avatars.githubusercontent.com/u/17396138?v=3"
+// bio
+// :
+// null
+// blog
+// :
+// null
+// company
+// :
+// null
+// created_at
+// :
+// "2016-02-22T01:52:54Z"
+// email
+// :
+// null
+// events_url
+// :
+// "https://api.github.com/users/andrewfishersb/events{/privacy}"
+// followers
+// :
+// 10
+// followers_url
+// :
+// "https://api.github.com/users/andrewfishersb/followers"
+// following
+// :
+// 3
+// following_url
+// :
+// "https://api.github.com/users/andrewfishersb/following{/other_user}"
+// gists_url
+// :
+// "https://api.github.com/users/andrewfishersb/gists{/gist_id}"
+// gravatar_id
+// :
+// ""
+// hireable
+// :
+// true
+// html_url
+// :
+// "https://github.com/andrewfishersb"
+// id
+// :
+// 17396138
+// location
+// :
+// null
+// login
+// :
+// "andrewfishersb"
+// name
+// :
+// "Andrew Fisher"
+// organizations_url
+// :
+// "https://api.github.com/users/andrewfishersb/orgs"
+// public_gists
+// :
+// 0
+// public_repos
+// :
+// 85
+// received_events_url
+// :
+// "https://api.github.com/users/andrewfishersb/received_events"
+// repos_url
+// :
+// "https://api.github.com/users/andrewfishersb/repos"
+// site_admin
+// :
+// false
+// starred_url
+// :
+// "https://api.github.com/users/andrewfishersb/starred{/owner}{/repo}"
+// subscriptions_url
+// :
+// "https://api.github.com/users/andrewfishersb/subscriptions"
+// type
+// :
+// "User"
+// updated_at
+// :
+// "2016-10-03T16:08:29Z"
+// url
+// :
+// "https://api.github.com/users/andrewfishersb"
