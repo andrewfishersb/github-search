8fb7545d9a05da81cd846ed8b901848830eca352
Andrew Fisher <andrewfishersb@gmail.com>
2016-10-18 16:07:17 -0700

    Update user.js
    got rid of commented out code

--- js/user.js
+++ js/user.js
@@ -12,7 +12,6 @@ User.prototype.getUserInformation = function (username, displayFunction) {
   });
 };
 
-//could reuse this method for followers and following and just add a third parameter
 User.prototype.getOtherInformation = function(typeOfInformation,username, displayFunction){
   $.get('https://api.github.com/users/'+username+'/'+typeOfInformation+'?access_token=' + apiKey).then(function(response){
     displayFunction(username,response);
@@ -21,117 +20,5 @@ User.prototype.getOtherInformation = function(typeOfInformation,username, displa
   });
 };
 
-// User.prototype.getFollowers = function (username, displayFunction) {
-//   $.get('https://api.github.com/users/'+username+'/followers?access_token=' + apiKey).then(function(response){
-//     displayFunction(username,response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
-//
-// User.prototype.getFollowing = function (username, displayFunction) {
-//   $.get('https://api.github.com/users/'+username+'/following?access_token=' + apiKey).then(function(response){
-//     displayFunction(username,response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
-
-
 
 exports.userModule = User;
-// f2f8bb3db6ffc1a7de0808320421143b7ce7835d
-//https://api.github.com/users/repos?access_token=
-
-
-// Object {login: "andrewfishersb", id: 17396138, avatar_url: "https://avatars.githubusercontent.com/u/17396138?v=3", gravatar_id: "", url: "https://api.github.com/users/andrewfishersb"…}
-// avatar_url
-// :
-// "https://avatars.githubusercontent.com/u/17396138?v=3"
-// bio
-// :
-// null
-// blog
-// :
-// null
-// company
-// :
-// null
-// created_at
-// :
-// "2016-02-22T01:52:54Z"
-// email
-// :
-// null
-// events_url
-// :
-// "https://api.github.com/users/andrewfishersb/events{/privacy}"
-// followers
-// :
-// 10
-// followers_url
-// :
-// "https://api.github.com/users/andrewfishersb/followers"
-// following
-// :
-// 3
-// following_url
-// :
-// "https://api.github.com/users/andrewfishersb/following{/other_user}"
-// gists_url
-// :
-// "https://api.github.com/users/andrewfishersb/gists{/gist_id}"
-// gravatar_id
-// :
-// ""
-// hireable
-// :
-// true
-// html_url
-// :
-// "https://github.com/andrewfishersb"
-// id
-// :
-// 17396138
-// location
-// :
-// null
-// login
-// :
-// "andrewfishersb"
-// name
-// :
-// "Andrew Fisher"
-// organizations_url
-// :
-// "https://api.github.com/users/andrewfishersb/orgs"
-// public_gists
-// :
-// 0
-// public_repos
-// :
-// 85
-// received_events_url
-// :
-// "https://api.github.com/users/andrewfishersb/received_events"
-// repos_url
-// :
-// "https://api.github.com/users/andrewfishersb/repos"
-// site_admin
-// :
-// false
-// starred_url
-// :
-// "https://api.github.com/users/andrewfishersb/starred{/owner}{/repo}"
-// subscriptions_url
-// :
-// "https://api.github.com/users/andrewfishersb/subscriptions"
-// type
-// :
-// "User"
-// updated_at
-// :
-// "2016-10-03T16:08:29Z"
-// url
-// :
-// "https://api.github.com/users/andrewfishersb"
